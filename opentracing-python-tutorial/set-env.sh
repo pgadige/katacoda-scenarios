30dbf1a7065aacc6f50d194982ba398c3799a685
Pooja Gadige <poojagadige@gmail.com>
2018-05-17 21:08:16 +0530

    Change how exercise directory is created

--- opentracing-python-tutorial/set-env.sh
+++ opentracing-python-tutorial/set-env.sh
@@ -4,6 +4,6 @@ cd /root/opentracing-tutorial/python &&
 
 python2.7 -m pip install -r requirements.txt &&
 
-mkdir -p /root/opentracing-tutorial/python/lesson01/exercise &&
+mkdir -p lesson01/exercise &&
 
-cd /root/opentracing-tutorial/python/lesson01/exercise
+cd ./lesson01/exercise
